923115dce4af7bbb56b5b83c8eb10ff69a8dbbed
SBSstudio <86954238+SBSstudio@users.noreply.github.com>
2021-09-07 06:47:06 -0700

    Update config.py

--- config.py
+++ config.py
@@ -5,8 +5,8 @@ import os
 
 
 class Config(object):
-    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "")
+    TG_BOT_TOKEN = os.environ.get("TG_BOT_TOKEN", "1997384120:AAG_7jxgLi_V1FvMof9msuo09hA2c9EAC6k")
 
-    APP_ID = int(os.environ.get("APP_ID", 12345))
+    APP_ID = int(os.environ.get("APP_ID", 3283847))
 
-    API_HASH = os.environ.get("API_HASH", "")
+    API_HASH = os.environ.get("API_HASH", "432c6eebf8df924b27be598c295c3692")
